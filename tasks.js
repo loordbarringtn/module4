964eff0930e377206981182d0aecdfe3ced718f5
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2022-12-16 21:53:13 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,30 +1,30 @@
-// const rain = Math.round(Math.random());
+const rain = Math.round(Math.random());
 
-// if (rain === 1) {
-//     console.log('Пошёл дождь. Возьмите зонт!');
-// } else if (rain === 0) {
-//     console.log('Дождя нет!');
-// }
+if (rain === 1) {
+    console.log('Пошёл дождь. Возьмите зонт!');
+} else if (rain === 0) {
+    console.log('Дождя нет!');
+}
 
-// const mathScore = prompt('Введите кол-во баллов по математике:');
-// const russianLanguageScore = prompt('Введите кол-во баллов по русскому языку:');
-// const InfTechnScore = prompt('Введите кол-во баллов по информатике: ');
+const mathScore = prompt('Введите кол-во баллов по математике:');
+const russianLanguageScore = prompt('Введите кол-во баллов по русскому языку:');
+const InfTechnScore = prompt('Введите кол-во баллов по информатике: ');
 
-// const totalMark = mathScore + russianLanguageScore + InfTechnScore;
+const totalMark = mathScore + russianLanguageScore + InfTechnScore;
 
-// if (totalMark>=265) {
-//     console.log('Поздравляю, вы поступили на бюджет!');
-// } else {
-//     console.log('К сожалению Вы не поступили!');
-// }
+if (totalMark>=265) {
+    console.log('Поздравляю, вы поступили на бюджет!');
+} else {
+    console.log('К сожалению Вы не поступили!');
+}
 
 
-// const atmMoney = 9000;
+const atmMoney = 9000;
 
-// const withdrawAmmount = prompt('Введите сумму для выдачи');
+const withdrawAmmount = prompt('Введите сумму для выдачи');
 
-// if (withdrawAmmount % 100 ===0 && withdrawAmmount <= 9000) {
-//     console.log('Выдача денег');
-// } else {
-//     console.log('В банкомате нет денежных средств');
-// }
+if (withdrawAmmount % 100 ===0 && withdrawAmmount <= 9000) {
+    console.log('Выдача денег');
+} else {
+    console.log('В банкомате нет денежных средств');
+}
